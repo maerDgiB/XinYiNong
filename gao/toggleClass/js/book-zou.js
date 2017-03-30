d33e7406141340a0c2358330168db634f5f44903
Kevin <1041874421@qq.com>
2017-03-30 23:30:07 +0800

    重构

--- gao/toggleClass/js/book-zou.js
+++ gao/toggleClass/js/book-zou.js
@@ -1,8 +1,31 @@
-$(".circle").on("click",function(){
-	// console.log($(this).hasClass("selected"));
-	if(!$(this).hasClass("selected")){
-		$(this).addClass("selected")
+/*
+面向过程的写法
+	$(".circle").on("click",function(){
+		// console.log($(this).hasClass("selected"));
+		if(!$(this).hasClass("selected")){
+			$(this).addClass("selected")
+		}else{
+			$(this).removeClass("selected")
+		}
+	})
+*/
+
+
+/*
+ * 考虑到可能今后会用到类似方法
+ * 1. 将重复的封装（形似）
+ */
+
+function toggle(ele,className){
+	// 先判断这个元素有没有这个类
+	var hasClass =  ele.hasClass(className);
+	if(!hasClass){
+		ele.addClass(className)
 	}else{
-		$(this).removeClass("selected")
+		ele.removeClass(className)
 	}
+}
+
+$(".circle").on("click",function(){
+	toggle($(this),"selected")
 })
--- gao/toggleClass/js/book.js
+++ gao/toggleClass/js/book.js
@@ -3,3 +3,14 @@ $(function(){
 $(this).toggleClass("selected");
   })
 })
+
+/*
+ * $(function(){})
+ * 这个当你在body前面引用的JS的使用才要用到
+ * 因为会在DOM结构渲染好时候绑定事件
+ * 我说过不要用.click这样的事件绑定
+ * $(this) 不理解看我的   https://baixiaoji.github.io/2017/03/19/%E6%80%BB%E7%BB%93this%E6%8C%87%E5%90%91%E9%97%AE%E9%A2%98/
+ * 看book-zou文件
+ *  11/04 你还有 11 天 
+ *  不要再找借口了  
+ */
